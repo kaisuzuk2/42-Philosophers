13855ecbc801fc7cbdebfbc1605ac10456007443
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-11-24 09:17:15 +0900

    死亡フラグ取得関数を作成

--- philo/get.c
+++ philo/get.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 14:30:17 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/24 09:09:21 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/24 09:16:41 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,15 @@ long get_ms_time(void)
 	gettimeofday(&tv, NULL);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
+
+t_bool get_is_dead(t_philo *philo)
+{
+	t_bool res;
+
+	pthread_mutex_lock(&philo->table->m_is_dead);
+	res = philo->table->is_dead;
+	pthread_mutex_unlock(&philo->table->m_is_dead);
+}
+
+
+
--- philo/thread_routine.c
+++ philo/thread_routine.c
@@ -6,11 +6,20 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 13:35:27 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/24 08:51:05 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/24 09:16:43 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+/*
+philo_routine
+1) is_dead flg check
+2) 
+
+monitor_routine
+
+*/
 
 void	*philo_routine(void *arg)
 {
