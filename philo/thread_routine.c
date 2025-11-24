0ff417a8699fddc4b784c1754ece52fc34516866
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-11-24 10:47:20 +0900

    まずは動かしてみよう

--- philo/thread_routine.c
+++ philo/thread_routine.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 13:35:27 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/24 09:16:43 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/24 10:44:53 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 /*
 philo_routine
 1) is_dead flg check
-2) 
+2) get fork
+3) eat
+4) sleep
+5) thinking
 
 monitor_routine
 
@@ -26,6 +29,11 @@ void	*philo_routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	set_last_eat_time(philo, philo->table->start_time);
+	pthread_mutex_lock(&philo->table->m_print);
+	printf("%d is start\n", philo->id);
+	pthread_mutex_unlock(&philo->table->m_print);
+	return (NULL);
 }
 
 void	*monitor_routine(void *arg)
@@ -33,6 +41,8 @@ void	*monitor_routine(void *arg)
 	t_monitor *mon;
 
 	mon = (t_monitor *)arg;
-	set_start_time(mon, get_ms_time());
-	
+	pthread_mutex_lock(&mon->m_print);
+	printf("monitor is start\n");
+	pthread_mutex_unlock(&mon->m_print);
+	return (NULL);
 }
