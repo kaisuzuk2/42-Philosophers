f61aa49cea7f1e0d17505a32f374bfa86bc5ca08
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-11-25 10:19:14 +0900

    みすった

--- philo/set.c
+++ philo/set.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 13:39:27 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/25 10:17:58 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/25 10:18:59 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	set_last_eat_time(t_philo *philo, long time)
 {
 	pthread_mutex_lock(&philo->last_eat_time.lock);
-	philo->last_eat_time = time;
+	philo->last_eat_time.value = time;
 	pthread_mutex_unlock(&philo->last_eat_time.lock);
 }
 
@@ -29,7 +29,7 @@ void	set_eat_count(t_philo *philo)
 void	set_died_flg(t_monitor *mon)
 {
 	pthread_mutex_lock(&mon->is_died.lock);
-	mon->is_died = TRUE;
+	mon->is_died.is_flg = TRUE;
 	pthread_mutex_unlock(&mon->is_died.lock);
 }
 
