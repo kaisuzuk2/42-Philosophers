bba013c4895636ea6882e6b2ed171a92a78f4508
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-11-26 13:33:53 +0900

    fork取得・破棄関数を作成

--- philo/thread_routine.c
+++ philo/thread_routine.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 13:35:27 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/11/26 11:57:51 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/11/26 13:27:28 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,35 @@ monitor_routine
 
 */
 
-static t_bool	do_eat(t_philo *philo, t_bool is_must_eat)
+static void	take_fork(t_philo *philo)
 {
-	if (is_died(&philo->table->is_died))
-		return (FALSE);
 	pthread_mutex_lock(&philo->table->fork_lock[philo->l_fork]);
 	print_state(philo, ST_FORK);
 	pthread_mutex_lock(&philo->table->fork_lock[philo->r_fork]);
 	print_state(philo, ST_FORK);
+}
+
+static void	release_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->table->fork_lock[philo->l_fork]);
+	pthread_mutex_unlock(&philo->table->fork_lock[philo->r_fork]);
+}
+
+static t_bool	do_eat(t_philo *philo, t_bool is_must_eat)
+{
+	if (is_died(&philo->table->is_died))
+		return (FALSE);
+	take_fork(philo);
 	set_last_eat_time(&philo->last_eat_time, get_current_mstime());
 	if (is_died(&philo->table->is_died))
 	{
-		pthread_mutex_unlock(&philo->table->fork_lock[philo->l_fork]);
-		pthread_mutex_unlock(&philo->table->fork_lock[philo->r_fork]);
+		release_forks(philo);
 		return (FALSE);
 	}
 	print_state(philo, ST_EAT);
 	usleep(philo->table->conf->time_to_eat);
 	set_eat_count(&philo->eat_count);
-	pthread_mutex_unlock(&philo->table->fork_lock[philo->l_fork]);
-	pthread_mutex_unlock(&philo->table->fork_lock[philo->r_fork]);
+	release_forks(philo);
 	if (is_died(&philo->table->is_died))
 		return (FALSE);
 	if (is_must_eat)
