3920b8155f0206b48b48be6c9caee3ab4a51b0f3
kaisuzuk <szkakg@gmail.com>
2025-12-01 08:54:32 +0000

    routine_philoファイルを分割

--- philo/srcs/routine_philo.c
+++ philo/srcs/routine_philo.c
@@ -12,50 +12,12 @@
 
 #include "philo.h"
 
-static void	release_forks(t_philo *philo)
-{
-	pthread_mutex_unlock(&philo->table->fork_lock[philo->l_fork]);
-	pthread_mutex_unlock(&philo->table->fork_lock[philo->r_fork]);
-}
+// routine_philo_utils.c
+void	release_forks(t_philo *philo);
+void select_forks(t_philo *philo, pthread_mutex_t **first, pthread_mutex_t **second);
+t_bool lock_fork(t_philo *philo, pthread_mutex_t *fork);
+t_bool	take_forks(t_philo *philo);
 
-static void select_forks(t_philo *philo, pthread_mutex_t **first, pthread_mutex_t **second)
-{
-	*first = &philo->table->fork_lock[philo->r_fork];
-	*second = &philo->table->fork_lock[philo->l_fork];
-	if (philo->id % 2)
-	{
-		*first = &philo->table->fork_lock[philo->l_fork];
-		*second = &philo->table->fork_lock[philo->r_fork];
-	}
-}
-
-static t_bool lock_fork(t_philo *philo, pthread_mutex_t *fork)
-{
-	pthread_mutex_lock(fork);
-	if (is_died(&philo->table->is_died))
-	{
-		pthread_mutex_unlock(fork);
-		return (FALSE);
-	}
-	print_state(philo, ST_FORK);
-	return (TRUE);
-}
-
-static t_bool	take_forks(t_philo *philo)
-{
-	pthread_mutex_t	*first_fork;
-	pthread_mutex_t	*second_fork;
-
-	select_forks(philo, &first_fork, &second_fork);
-	if (!lock_fork(philo, first_fork))
-		return (FALSE);
-	if (!lock_fork(philo, second_fork))
-	{
-		pthread_mutex_unlock(first_fork);
-		return (FALSE);
-	}
-	return (TRUE);
-}
 
 static t_bool	do_eat(t_philo *philo, t_bool is_must_eat)
 {
---
+++ philo/srcs/routine_philo_utils.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine_philo_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-12-01 08:52:15 by root              #+#    #+#             */
+/*   Updated: 2025-12-01 08:52:15 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	release_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->table->fork_lock[philo->l_fork]);
+	pthread_mutex_unlock(&philo->table->fork_lock[philo->r_fork]);
+}
+
+void select_forks(t_philo *philo, pthread_mutex_t **first, pthread_mutex_t **second)
+{
+	*first = &philo->table->fork_lock[philo->r_fork];
+	*second = &philo->table->fork_lock[philo->l_fork];
+	if (philo->id % 2)
+	{
+		*first = &philo->table->fork_lock[philo->l_fork];
+		*second = &philo->table->fork_lock[philo->r_fork];
+	}
+}
+
+t_bool lock_fork(t_philo *philo, pthread_mutex_t *fork)
+{
+	pthread_mutex_lock(fork);
+	if (is_died(&philo->table->is_died))
+	{
+		pthread_mutex_unlock(fork);
+		return (FALSE);
+	}
+	print_state(philo, ST_FORK);
+	return (TRUE);
+}
+
+t_bool	take_forks(t_philo *philo)
+{
+	pthread_mutex_t	*first_fork;
+	pthread_mutex_t	*second_fork;
+
+	select_forks(philo, &first_fork, &second_fork);
+	if (!lock_fork(philo, first_fork))
+		return (FALSE);
+	if (!lock_fork(philo, second_fork))
+	{
+		pthread_mutex_unlock(first_fork);
+		return (FALSE);
+	}
+	return (TRUE);
+}
