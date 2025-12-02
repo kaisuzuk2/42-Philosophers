4bf84e0846e1cd577066a3716526a6d5937f7903
Kaisei Suzuki <kaisuzuk@c1r6s1.42tokyo.jp>
2025-12-02 15:45:17 +0900

    うーん

--- philo/srcs/get.c
+++ philo/srcs/get.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 14:30:17 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/12/02 13:29:21 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/12/02 15:45:08 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ long long	get_current_mstime(void)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return ((long long)(tv.tv_sec * 1000LL) + (long long)(tv.tv_usec / 1000LL));
+	return ((long long)(tv.tv_sec * 1000) + (long long)(tv.tv_usec / 1000));
 }
 
 long long	get_last_eat_time(t_atomic_long *last_eat_time)
